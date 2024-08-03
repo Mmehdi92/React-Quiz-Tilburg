32d0806278faa77338643abe751908bd4ed9d0c9
Mehdi Zymberi <zymberi.mz@gmail.com>
2024-08-03 14:25:34 +0200

    finished quiz

---
+++ src/questions.js
@@ -0,0 +1,147 @@
+// export default [
+//     {
+//       id: 'q1',
+//       text: 'Which of the following definitions best describes React.js?',
+//       answers: [
+//         'A library to build user interfaces with help of declarative code.',
+//         'A library for managing state in web applications.',
+//         'A framework to build user interfaces with help of imperative code.',
+//         'A library used for building mobile applications only.',
+//       ],
+//     },
+//     {
+//       id: 'q2',
+//       text: 'What purpose do React hooks serve?',
+//       answers: [
+//         'Enabling the use of state and other React features in functional components.',
+//         'Creating responsive layouts in React applications.',
+//         'Handling errors within the application.',
+//         'Part of the Redux library for managing global state.',
+//       ],
+//     },
+//     {
+//       id: 'q3',
+//       text: 'Can you identify what JSX is?',
+//       answers: [
+//         'A JavaScript extension that adds HTML-like syntax to JavaScript.',
+//         'A JavaScript library for building dynamic user interfaces.',
+//         'A specific HTML version that was explicitly created for React.',
+//         'A tool for making HTTP requests in a React application.',
+//       ],
+//     },
+//     {
+//       id: 'q4',
+//       text: 'What is the most common way to create a component in React?',
+//       answers: [
+//         'By defining a JavaScript function that returns a renderable value.',
+//         'By defining a custom HTML tag in JavaScript.',
+//         'By creating a file with a .jsx extension.',
+//         'By using the "new" keyword followed by the component name.',
+//       ],
+//     },
+//     {
+//       id: 'q5',
+//       text: 'What does the term "React state" imply?',
+//       answers: [
+//         'An object in a component that holds values and may cause the component to render on change.',
+//         'The lifecycle phase a React component is in.',
+//         'The overall status of a React application, including all props and components.',
+//         'A library for managing global state in React applications.',
+//       ],
+//     },
+//     {
+//       id: 'q6',
+//       text: 'How do you typically render list content in React apps?',
+//       answers: [
+//         'By using the map() method to iterate over an array of data and returning JSX.',
+//         'By using the for() loop to iterate over an array of data and returning JSX.',
+//         'By using the forEach() method to iterate over an array of data and returning JSX.',
+//         'By using the loop() method to iterate over an array of data and returning JSX.',
+//       ],
+//     },
+//     {
+//       id: 'q7',
+//       text: 'Which approach can NOT be used to render content conditionally?',
+//       answers: [
+//         'Using a the #if template syntax.',
+//         'Using a ternary operator.',
+//         'Using the && operator.',
+//         'Using an if-else statement.',
+//       ],
+//     },
+//   ];
+
+
+
+export default [
+  {
+    id: 'q1',
+    text: 'Welke van de volgende bezienswaardigheden vind je in Tilburg?',
+    answers: [
+      'De Efteling',
+      'De Tilburgse Kermis',
+      'De Euromast',
+      'Het Rijksmuseum',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'Wat is de bijnaam van het voetbalteam van Tilburg, Willem II?',
+    answers: [
+      'De Tricolores',
+      'De Leeuwen',
+      'De Draken',
+      'De Kanaries',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'Welke universiteit is gevestigd in Tilburg?',
+    answers: [
+      'Tilburg University',
+      'Erasmus Universiteit',
+      'Radboud Universiteit',
+      'Technische Universiteit Delft',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'Welke bekende jaarlijks terugkerende culturele gebeurtenis vindt plaats in Tilburg?',
+    answers: [
+      'Festival Mundial',
+      'Pinkpop',
+      'Oerol',
+      'Lowlands',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'Wat is de historische band van Tilburg met de textielindustrie?',
+    answers: [
+      'Tilburg was een belangrijk textielcentrum.',
+      'Tilburg stond bekend om zijn schoenenproductie.',
+      'Tilburg was een centrum voor scheepsbouw.',
+      'Tilburg had geen relatie met de textielindustrie.',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'Hoe heet het bekende natuurgebied dat vlakbij Tilburg ligt?',
+    answers: [
+      'De Loonse en Drunense Duinen',
+      'De Hoge Veluwe',
+      'De Biesbosch',
+      'De Oostvaardersplassen',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'Welke Tilburgse straat is bekend vanwege zijn vele cafés en restaurants?',
+    answers: [
+      'De Heuvelstraat',
+      'De Lijnbaan',
+      'De Nieuwe Binnenweg',
+      'De Kalverstraat',
+    ],
+  },
+];
